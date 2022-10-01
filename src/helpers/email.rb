bee07571deeac6f9c15c5d4e92970aa3b8752dee
James MacAdie <james@macadie.co.uk>
2022-10-01 11:10:13 +0100

    Refactor e-mail configs

--- src/helpers/email.rb
+++ src/helpers/email.rb
@@ -1,41 +1,43 @@
 module Email
     def send_email(subject, body, to = nil)
+        config = App::settings.email
+        is_prod = App::settings.production?
         params = {
-            :to => get_to(to),
+            :to => get_to(to, config, is_prod),
             :subject => subject,
-            :body => get_body(body, to)
+            :body => get_body(body, to, is_prod)
         }
-        params = params.merge(get_transport())
+        params = params.merge(get_transport(config))
         Pony.mail(params)
     end
 
-    private def get_to(to)
-        if App::settings.production?
-            to.nil? ? App::settings.email_default_to : to
+    private def get_to(to, config, is_prod)
+        if is_prod
+            to.nil? ? config['default_to'] : to
         else
-            App::settings.email_to_sub
+            config['sub_to']
         end
     end
 
-    private def get_body(body, to)
-        if App::settings.production?
+    private def get_body(body, to, is_prod)
+        if is_prod
             body
         else
             'App sending to: ' + to.to_s + '   ' + body
         end
     end
 
-    private def get_transport()
+    private def get_transport(config)
         {
             :via => :smtp,
             :via_options => {
                 :address              => 'smtp.gmail.com',
-                :port                 => '587',
+                :port                 => 587,
                 :enable_starttls_auto => true,
-                :user_name            => App::settings.email_username,
-                :password             => App::settings.email_password,
+                :user_name            => config['gmail_username'],
+                :password             => config['app_password'],
                 :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-                :domain               => 'julianrimet.com'
+                :domain               => config['app_domain']
             }
         }
     end
