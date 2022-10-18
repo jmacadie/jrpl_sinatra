53d1b25a5cfb4d7dc40c7978045e76d113ca7804
James MacAdie <james@macadie.co.uk>
2022-10-18 22:51:51 +0100

    Refactor match controller
    Shortened the main route methods and extracted the validation logic
    to private methods

--- src/controllers/match.rb
+++ src/controllers/match.rb
@@ -9,56 +9,63 @@ class App < Sinatra::Application
     erb :match
   end
 
-  # rubocop:todo Metrics/BlockLength
   post '/match/add_prediction' do
     require_signed_in_user
     match_id = params[:match_id].to_i
-    move_next = to_bool(params[:next])
     home_prediction = params[:home_team_prediction].to_f
     away_prediction = params[:away_team_prediction].to_f
+    move_next = to_bool(params[:next])
     load_match_details(match_id)
-    session[:message] =
-      prediction_error(@match, home_prediction, away_prediction)
-    if session[:message]
-      status 422
-      erb :match
-    else
-      @storage.add_prediction(
-        session[:user_id],
-        match_id,
-        home_prediction.to_i,
-        away_prediction.to_i
-      )
-      session[:message] = 'Prediction submitted'
-      match_id = @next_match[:match_id] if move_next
-      redirect redirect_url(match_id, move_next: move_next)
-    end
+    return erb :match unless
+      validate_prediction(home_prediction, away_prediction)
+    @storage.add_prediction(
+      session[:user_id],
+      match_id,
+      home_prediction.to_i,
+      away_prediction.to_i
+    )
+    session[:message] = 'Prediction submitted'
+    match_id = @next_match[:match_id] if move_next
+    redirect redirect_url(match_id, move_next: move_next)
   end
-  # rubocop:enable Metrics/BlockLength
 
   post '/match/add_result' do
     require_signed_in_as_admin
+    match_id = params[:match_id].to_i
     home_score = params[:home_score].to_f
     away_score = params[:away_score].to_f
-    match_id = params[:match_id].to_i
     load_match_details(match_id)
-    session[:message] = match_result_error(@match, home_score, away_score)
-    if session[:message]
-      status 422
-      erb :match
-    else
-      home_score = home_score.to_i
-      away_score = away_score.to_i
-      @storage.add_result(
-        match_id, home_score, away_score, session[:user_id]
-      )
-      update_scoreboard(match_id, home_score, away_score)
-      session[:message] = 'Result submitted'
-      redirect redirect_url(match_id)
-    end
+    return erb :match unless
+      validate_result(home_score, away_score)
+    home_score = home_score.to_i
+    away_score = away_score.to_i
+    @storage.add_result(
+      match_id, home_score, away_score, session[:user_id]
+    )
+    update_scoreboard(match_id, home_score, away_score)
+    session[:message] = 'Result submitted'
+    redirect redirect_url(match_id)
   end
 
   private
+
+  def validate_prediction(home, away)
+    session[:message] = prediction_error(@match, home, away)
+    if session[:message]
+      status 422
+      return false
+    end
+    true
+  end
+
+  def validate_result(home, away)
+    session[:message] = match_result_error(@match, home, away)
+    if session[:message]
+      status 422
+      return false
+    end
+    true
+  end
 
   def redirect_url(match_id, move_next: false)
     root = "/match/#{match_id}"
