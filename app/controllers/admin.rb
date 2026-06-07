abbb3e1a4588ed555fac8d9f35e58062920bfb01
James MacAdie <j.macadie@ttg.co.uk>
2026-06-07 19:47:37 +0100

    Use locals in rest of views
    Finish the job from the last two commits

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -2,8 +2,10 @@ class App < Sinatra::Application
   get '/admin' do
     require_signed_in_as_admin
     page = settings.admin_page_service.call
-    @users = page.users
-    @roles = page.roles
-    erb :admin
+    erb :admin,
+        locals: {
+          users: page.users,
+          roles: page.roles
+        }
   end
 end
